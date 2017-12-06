2b2f18c27a39932be95886156e8f1f636f1a1c60
Aman Agarwal <aman_k_agarwal@hotmail.com>
2017-12-06 00:42:38 -0800

    Progress

--- scripts/win-rate-comparison.R
+++ scripts/win-rate-comparison.R
@@ -27,9 +27,34 @@ winrateRole <- function(role) {
   return (left_join(blue, red))
 }
 
+champPlayRateBlue <- function(role) {
+  blue.role <- paste0("blue", role)
+  blue.champ <- paste0("blue", role, "Champ")
+  blue <- league.data %>%
+    group_by_(blue.role, blue.champ) %>%
+    summarise(games.played = n()) %>%
+    arrange_(blue.role, blue.champ) %>%
+    select(player = blue.role, champion = blue.champ, games.played) 
+  return (blue)
+}
+
+champPlayRateRed <- function(role) {
+  red.role <- paste0("red", role)
+  red.champ <- paste0("red", role, "Champ")
+  red <- league.data %>%
+    group_by_(red.role, red.champ) %>%
+    summarise(games.played = n()) %>%
+    arrange_(red.role, red.champ) %>%
+    select(player = red.role, champion = red.champ, games.played)
+  return(red)
+}
+
+playratePlot <- function(role, player1, player2) {
+  
+}
 # This function makes a stacked bar plot of two selected players and their respective win rates on 
 # the red and blue sides.
-makePlot <- function(role, player1, player2) {
+winratePlot <- function(role, player1, player2) {
   tmp <- winrateRole(role)
   players <- c(player1, player2)
   tmp <- tmp %>%
