37627d5566333b2be1cee30dd7ac1460153bd283
Aman Agarwal <aman_k_agarwal@hotmail.com>
2017-12-05 23:43:51 -0800

    some changes

--- scripts/prediction.R
+++ scripts/prediction.R
@@ -4,14 +4,21 @@ library("dplyr")
 data <- read.csv("data/games.csv", stringsAsFactors = FALSE)
 comp.data <- read.csv("data/_LeagueofLegends.csv", stringsAsFactors = FALSE)
 
+listToLength <- function(col) {
+  col = eval(parse(text = col))
+  return (unlist(lapply(strsplit(data$col, ","), length)))
+}
+comp.data$bTowers <- listToLength(bTowers)
+comp.data$bDragons <- listToLength(bDragons)
+comp.data$bInhibs <- listToLength(bInhibs)
+comp.data$rTowers <- listToLength(rTowers)
+comp.data$rDragons <- listToLength(rDragons)
+comp.data$rInhibs <-listToLength(rInhibs)
+
 # 1 for blue win, 0 for red win
 comp.data <- comp.data %>%
   select(Winner = bResult, Tower1 = bTowers, Dragon1 = bDragons, Inhib1 = bInhibs,
          Tower2 = rTowers, Dragon2 = rDragons, Inhib2 = rInhibs)
-
-# Need to mutate all the rBarons, blah blah with the length of the list (num of times they got the objective data)
-# Otherwise I don't think I can calculate the correlation between the two. Yes I cannot calculate because they have to 
-# be numeric values. Otherwise I cannot calculate it.
 cor1.data <- as.matrix(comp.data)
 cor1 <- cor(cor1.data)
 corrplot.mixed(cor1, "Competitive games correlations")
@@ -26,9 +33,3 @@ cor.data <- data %>%
 cor.data <- as.matrix(cor.data)
 cor <- cor(cor.data)
 corrplot.mixed(cor, title = "Correlation between objectives and game outcome")
-
-a1 <- c("Hello", "World")
-b1 <- c("[1, 2]", "[1,2,3]")
-c1 <- c("[1, 2, 3, 3]", "[1, 4, 5, 6]")
-
-data <- data.frame(a1, b1, c1)
